545e08553ffc8b17fdb610c5a670f2b5a503c967
brunopgal <brunopgal2006@gmail.com>
2026-06-12 00:05:40 -0300

    feat: rota /relatorios

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import Construtoras from "./pages/Construtoras";
 import Atividades from "./pages/Atividades";
 import ProspeccaoIA from "./pages/ProspeccaoIA";
 import Pessoas from "./pages/Pessoas";
+import Relatorios from "./pages/Relatorios";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -44,6 +45,7 @@ const App = () => (
                     <Route path="/construtoras" element={<Construtoras />} />
                     <Route path="/atividades/:id" element={<Atividades />} />
                     <Route path="/pessoas" element={<Pessoas />} />
+                    <Route path="/relatorios" element={<Relatorios />} />
                     <Route path="/prospeccao-ia" element={<ProspeccaoIA />} />
                     <Route path="/nova-obra" element={<NovaObra />} />
                     <Route path="*" element={<NotFound />} />
