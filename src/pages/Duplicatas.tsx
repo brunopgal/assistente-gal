6baf152aba0cf444bf38551335369e8b525aac28
Bruno Gal <brunopgal@gmail.com>
2026-06-24 08:21:09 -0300

    chore: force redeploy in Lovable

--- src/pages/Duplicatas.tsx
+++ src/pages/Duplicatas.tsx
@@ -1,3 +1,4 @@
+// Forced rebuild trigger
 import { useEffect, useState } from "react";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
